0cbd026fa3280999ab68ac0aa67448fc692f08be
Sejoon Lim <sejoon.lim@Sejoons-MacBook-Pro.local>
2019-11-18 20:52:37 +0900

    Updated App.js with a complex object

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import './App.css';
 
 class  App extends Component {
   render() {
-    let message = 'Welcome to the Road to learn React!!';
+    const message = 'Welcome to the Road to learn React!!';
+    const user = {
+      first: 'Sejoon',
+      last: 'Lim'
+    };
+
     return (
         <div className="App">
           <h2>{message}</h2>
+          <h5>Hello, {user.first} {user.last}</h5>
         </div>
     );
   }
