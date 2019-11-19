c52c912b364cdfb38545d260090b1fb5b9c21a55
Sejoon Lim <sejoon.lim@Sejoons-MacBook-Pro.local>
2019-11-19 20:29:56 +0900

    Added search functionality

--- src/App.js
+++ src/App.js
@@ -24,7 +24,8 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            articles
+            articles,
+            searchTerm: ''
         };
     }
     
@@ -34,33 +35,49 @@ class App extends Component {
         return (
             <div className="App">
                 <h2>{message}</h2>
+                <form>
+                    <input type="text" onChange={this.onSearchChange}/>
+                </form>
                 {this.renderItems()}
             </div>
         );
     }
     
     renderItems = () => {
-        return this.state.articles.map(item =>
-            <div key={item.objectId}>
-                <span>
-                    <a href={item.url}>{item.title}</a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.commentCount}</span>
-                <span>{item.points}</span>
-                <span>
-                    <button onClick={() => this.onDismiss(item.objectId)} type="button">
-                        Dismiss
-                    </button>
-                </span>
-            </div>
-        );
+        return this.state.articles
+            .filter(item => this.isSearchedItem(item))
+            .map(item =>
+                <div key={item.objectId}>
+                    <span>
+                        <a href={item.url}>{item.title}</a>
+                    </span>
+                        <span>{item.author}</span>
+                        <span>{item.commentCount}</span>
+                        <span>{item.points}</span>
+                        <span>
+                        <button onClick={() => this.onDismiss(item.objectId)} type="button">
+                            Dismiss
+                        </button>
+                    </span>
+                </div>
+            );
+    };
+    
+    isSearchedItem = (item) => {
+        const keyword = this.state.searchTerm;
+        return item.title.toLowerCase().includes(keyword.toLowerCase());
+    };
+    
+    onSearchChange = (event) => {
+        this.setState({
+            searchTerm: event.target.value
+        });
     };
     
     onDismiss = (objectId) => {
         const updated = this.state.articles.filter(item => item.objectId !== objectId);
         this.setState({articles: updated});
-    }
+    };
 }
 
 export default App;
