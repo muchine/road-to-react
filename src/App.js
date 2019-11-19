4f0699899569d0d7b33af5f3854f7b4839559646
Sejoon Lim <sejoon.lim@Sejoons-MacBook-Pro.local>
2019-11-19 19:57:03 +0900

    Added onDismiss button click listener

--- src/App.js
+++ src/App.js
@@ -1,21 +1,66 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-class  App extends Component {
-  render() {
-    const message = 'Welcome to the Road to learn React!!';
-    const user = {
-      first: 'Sejoon',
-      last: 'Lim'
-    };
+const articles = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org',
+        author: 'Jordan Walke',
+        commentCount: 3,
+        points: 4,
+        objectId: 0
+    }, {
+        title: 'Redux',
+        url: 'https://redux.js.org',
+        author: 'Dan Abramov, Andrew Clark',
+        commentCount: 2,
+        points: 5,
+        objectId: 1
+    }
+];
 
-    return (
-        <div className="App">
-          <h2>{message}</h2>
-          <h5>Hello, {user.first} {user.last}</h5>
-        </div>
-    );
-  }
+class App extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            articles
+        };
+    }
+    
+    render() {
+        const message = 'Welcome to the Road to learn React!!';
+        
+        return (
+            <div className="App">
+                <h2>{message}</h2>
+                {this.renderItems()}
+            </div>
+        );
+    }
+    
+    renderItems = () => {
+        return this.state.articles.map(item =>
+            <div key={item.objectId}>
+                <span>
+                    <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.commentCount}</span>
+                <span>{item.points}</span>
+                <span>
+                    <button onClick={() => this.onDismiss(item.objectId)} type="button">
+                        Dismiss
+                    </button>
+                </span>
+            </div>
+        );
+    };
+    
+    onDismiss = (objectId) => {
+        const updated = this.state.articles.filter(item => item.objectId !== objectId);
+        this.setState({articles: updated});
+    }
 }
 
 export default App;
